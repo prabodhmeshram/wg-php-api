e0d38c75045a223dc76908011f2a1a4d1d5ded8c
Prabodh Meshram <prabodh.meshram7@gmail.com>
2014-11-02 02:38:43 +0530

    First Local Commit

---
+++ WG.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+* Wrapper Class for WunderGround API
+*/
+class WG {
+
+// TODO:: declare members 
+
+
+ //Can be used to Set Authentication Params
+ public function __construct(){
+
+ }
+
+ private function setHeaders(){
+
+ }
+ 
+ public function processRequest(){
+ 	
+ }
+
+
+}
